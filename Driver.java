b985e57a75cd6a44f7781e9aac5320479151913c
Grace Seying <graceseying@jennas-iphone.reshall>
2016-04-28 15:23:34 -0500

    mergeSort

--- Driver.java
+++ Driver.java
@@ -6,13 +6,77 @@ public class Driver
 	{
 		int[] arrayOfNumbers = new int[10];
 		Driver.fillArrayWithRandomInts(arrayOfNumbers);
-		Driver.printIntArray(arrayOfNumbers);
-		Driver.bubbleSort(arrayOfNumbers);
-		Driver.printIntArray(arrayOfNumbers);
+		Driver.mergeSort(arrayOfNumbers);
 		
 	}
 	
-	//Homework
+	
+	static void mergeSort(int[] ar)
+	{
+		Driver.mergeSortHelper(ar, 0, ar.length-1);
+	}
+	
+	static void mergeSortHelper(int[] ar, int begin, int end)
+	{
+		Driver.display(ar, begin, end);
+		if(begin != end)
+		{
+			int leftBegin = begin;
+			int leftEnd = ((end-begin)/2 + begin);
+			int rightBegin = ((end-begin)/2 + begin) + 1; 
+			int rightEnd = end;
+			Driver.mergeSortHelper(ar, begin, ((end-begin)/2 + begin));
+			Driver.mergeSortHelper(ar, ((end-begin)/2 + begin) + 1,  end);
+			
+			int[] temp = new int [(rightEnd - leftBegin) + 1];
+			int currLeft = leftBegin;	
+			int currRight = rightBegin;
+			
+			for(int tempPos = 0; tempPos < temp.length; tempPos++)
+			{
+				if(currLeft < leftEnd)
+				{
+					temp[tempPos] = ar[currRight];
+					currRight++;
+				}
+				else if(currRight < rightEnd)
+				{
+					temp[tempPos] = ar[currLeft];
+					currLeft++;
+				}
+				else
+				{
+					if(ar[currLeft] < ar[currRight])
+					{
+						temp[tempPos] = ar[currLeft];
+						currLeft++;
+					}
+					else
+					{
+						temp[tempPos] = ar[currRight];
+						currRight++;
+					}
+				}
+			}
+			int pos = 0;
+			for(int i = leftBegin; i <= rightEnd; i++)
+			{
+				ar[i] = temp[pos];
+				pos++;
+			}
+			
+		}
+	}
+	
+	static void display(int[] ar, int begin, int end)
+	{
+		for(int i= begin; i <= end; i++)
+			{
+				System.out.print(ar[i] + " ");
+			}
+			System.out.println("");
+	}
+	
 	static void bubbleSort(int[] ar)
 	{
 		boolean swap = true;
